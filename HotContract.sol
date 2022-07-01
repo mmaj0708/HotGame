9398a1a94f94daa04604cf25fa4be08dcd3b1458
Melchior Maj <57007741+mmaj0708@users.noreply.github.com>
2022-07-01 18:13:33 +0200

    first working version

--- HotContract.sol
+++ HotContract.sol
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: MIT
 
-// problème de retard sur l'acquisition du random number
+/*
+** Author: Melchior Maj
+** Github: https://github.com/mmaj0708
+*/
+
+/*
+** This is a decentralized HEAD OR TAILS GAME with proven randomness
+**
+** It is not a live game, so you can submit a bet stored in the blockchain
+** and wait for someone to follow your bet, or you can follow a bet already submitted.
+** Submitters will always have the possibility to claim back there bet if no one followed,
+** as long as they claim back with the submitting wallet address.
+**
+** There is no limit on the size bets you can submit nor the number of bets.
+**
+** Random numbers are generated with Chainlink. Meaning, all the random numbers
+** are truly random, with provable randomness.
+** 
+** some fees are added to the bet and send to the contract owner, in order to add some LINK tokens
+** to the contract address, which is mandatory to generate fully random number.
+*/
+
+
+// utiliser une librairie pour accéder à tous les éléments de map ?
+// améliorer le delete en décalant les éléments de map ?
 
 pragma solidity ^0.8.7;
 
@@ -14,20 +38,23 @@ contract HotGame is VRFConsumerBaseV2, Ownable {
     event HotGameClaimBack(GameData); // event triggered for each game canceled
     event HotGameFinished(GameData, address); // event triggered when is played, second arg is the winner
 
+    enum gameStatus{ CREATED, RANDOM_REQUESTED }
+
     struct GameData {
-        string  gameId;     // used to link the back of this wonderful DApp
-        address submitter;  // first player who created the game
-        uint256 bet;        // bet value, set in wei
-        uint256 submitTime; // Timestamp of the launching game
+        string  gameId;             // used to link the back of this wonderful DApp
+        address submitter;          // first player who created the game
+        address player;             // first player who created the game
+        uint256 bet;                // bet value, set in wei
+        uint256 submitTime;         // Timestamp of the launching game
+        uint256 randomRequestId;    // request ID to identify for which game is the random number
+        gameStatus  status;         // used to know the state of game (created or in random requesting)
     }
     
     uint256 _fee;   // fee in wei
     address _owner;
-    GameData[] public games;
 
-    uint256 public randomInt;
-    // uint256 public prev_randomInt;
-    uint256 public requestId;
+    mapping(string => GameData) public games;
+    mapping(uint => GameData) public randReq;
 
     uint64 s_subscriptionId; // must be 210 for fuji-testnet
     address vrfCoordinator = 0x2eD832Ba664535e5886b75D64C46EB9a228C2610; // (fuji-testnet)
@@ -44,108 +71,75 @@ contract HotGame is VRFConsumerBaseV2, Ownable {
         COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
     }
 
-    function requestRandomWords() internal {
-        // Will revert if subscription is not set and funded.
+    function requestRandomWords() internal returns (uint256) {
+        uint256 requestId;
+
         requestId = COORDINATOR.requestRandomWords(
-        s_keyHash,
-        s_subscriptionId,
-        requestConfirmations,
-        callbackGasLimit,
-        numWords
+            s_keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
         );
+        return requestId;
     }
 
-    function fulfillRandomWords(uint256 /* requestId */,uint256[] memory randomWords) internal override {
-        // prev_randomInt = randomInt;
-        randomInt = (randomWords[0] % 100) + 1; // 1 to 100
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        uint256  randomInt = (randomWords[0] % 100) + 1;
+
+        if (randomInt > 50) { // submitter Win
+            payable(randReq[requestId].submitter).transfer(randReq[requestId].bet * 2);
+            emit HotGameFinished(randReq[requestId], randReq[requestId].submitter);
+        }
+        else { // player Win
+            payable(randReq[requestId].player).transfer(randReq[requestId].bet * 2);
+            emit HotGameFinished(randReq[requestId], randReq[requestId].player);
+        }
+        delete games[randReq[requestId].gameId];
+        delete randReq[requestId];
     }
 
-    function createHotGame(string memory gameId, uint256 bet) public payable returns(bool) {
-        require(_gameIdCheck(gameId), "gameId already exist");
-        require(msg.value - _fee == bet, "value is not fitting bet"); // bet announced is corresponding to msg.value
+    function createHotGame(string memory gameId, uint256 bet) public payable {
+        require(games[gameId].submitter == address(0), "game Id already exist");
+        require(msg.value - _fee == bet, "value is not fitting bet");
         payable(_owner).transfer(_fee);
         GameData memory newGame;
-        newGame = GameData(gameId, msg.sender, bet, block.timestamp);
+        newGame = GameData(gameId, msg.sender, address(0), bet, block.timestamp, 0, gameStatus.CREATED);
 
-        games.push(newGame);
+        games[gameId] = newGame;
         emit NewHotGame(newGame);
-        return true;
     }
 
     function playHotGame(string memory gameIdToPlay) public payable {
-        uint256 gameIndex = 0;
+        require(games[gameIdToPlay].submitter != address(0), "game Id not found");
+        require(msg.value - _fee == games[gameIdToPlay].bet, "value not fitting bet");
+        require(games[gameIdToPlay].status == gameStatus.CREATED, "Game is already processing (waiting for random number...)");
 
-        for (uint256 i = 0; i < games.length; i++) {
-            if (keccak256(abi.encodePacked(games[i].gameId)) == keccak256(abi.encodePacked(gameIdToPlay)))
-                gameIndex = i;
-        }
-        require(gameIndex >= 0, "game not found");    // gameId not found 
-        require(msg.value - _fee == games[gameIndex].bet, "value not fitting bet"); // if not => value not corresponding to bet
+        games[gameIdToPlay].player = msg.sender;
+
         payable(_owner).transfer(_fee);
 
-        // randInt = _random(); // 0 < randInt < 11499
-        requestRandomWords();
+        games[gameIdToPlay].randomRequestId = requestRandomWords();
+        randReq[games[gameIdToPlay].randomRequestId] = games[gameIdToPlay];
+        games[gameIdToPlay].status = gameStatus.RANDOM_REQUESTED;
 
-        // require(prev_randomInt != randomInt);
-
-        // submitter Win
-        if (randomInt > 5749) {
-            payable(games[gameIndex].submitter).transfer(games[gameIndex].bet * 2);
-            emit HotGameFinished(games[gameIndex], games[gameIndex].submitter);
-        }
-        // playerTwo Win
-        else {
-            payable(msg.sender).transfer(games[gameIndex].bet * 2);
-            emit HotGameFinished(games[gameIndex], msg.sender);
-        }
-        delete games[gameIndex];
-        _reorderArray(gameIndex);   // to order elements in games array and get correct length
+        // now player must wait for the random number to be send to fullfillRandomWords()
+        // from here winner will be determined and payout will be done
     }
 
-    function claimBack(string memory gameIdClaimed) public returns(bool) {
-        uint256 gameIndex = 0;
+    function claimBack(string memory gameIdClaimed) public {
+        require(games[gameIdClaimed].submitter != address(0), "game Id not found");
+        require(msg.sender == games[gameIdClaimed].submitter, "sender is not the submitter");
+        require(games[gameIdClaimed].status == gameStatus.CREATED);
 
-        for (uint256 i = 0; i < games.length; i++) {
-            if (keccak256(abi.encodePacked(games[i].gameId)) == keccak256(abi.encodePacked(gameIdClaimed)))
-                gameIndex = i;
-        }
-        require(gameIndex >= 0, "gameId not found");
-        require(msg.sender == games[gameIndex].submitter, "sender is not the submitter");
-        payable(msg.sender).transfer(games[gameIndex].bet);
+        payable(msg.sender).transfer(games[gameIdClaimed].bet);
 
-        // games[gameIndex].submitter = address(0) ?
-        emit HotGameClaimBack(games[gameIndex]);
-        delete games[gameIndex];
-        _reorderArray(gameIndex);   // to order elements in games array and get correct length
-        return true;
+        emit HotGameClaimBack(games[gameIdClaimed]);
+        delete games[gameIdClaimed];
     }
 
-    function setFee(uint256 newFee) onlyOwner public returns(bool) {
+    function setFee(uint256 newFee) onlyOwner public {
         _fee = newFee;
-        return (true);
-    }
-
-    function _reorderArray(uint emptyIndex) private {
-        for(uint i = emptyIndex; i < games.length-1; i++){
-            games[i] = games[i+1];      
-        }
-        games.pop();
-    }
-
-    function _random() public view returns (uint) { // will be truly random in HOTGAME V2 with chainlink
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, games.length))) / 10**(73);
-    }
-
-    function _gameIdCheck(string memory idToCheck) private view returns(bool) {
-        for (uint i = 0; i < games.length; i++) {
-            if (keccak256(abi.encodePacked(games[i].gameId)) == keccak256(abi.encodePacked(idToCheck)))
-                return false;
-        }
-        return true;
-    }
-
-    function getGames() public view returns(GameData[] memory){
-        return games;
     }
 
     function getContractStock() public view returns(uint){
